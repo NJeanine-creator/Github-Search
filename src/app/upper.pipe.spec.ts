efc8eaccdedaa5acb4b9291e0bc5852c2922a29f
NJeanine-creator <jnishimwe321@daviscollege.com>
2020-12-21 12:08:45 +0200

    Auto-generated commit

--- src/app/upper.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Upper} from './upper.pipe';
-
-describe('UpperPipe', () => {
-  it('create an instance', () => {
-    const pipe = new Upper();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  myApi:'a3ba208001e1310e82d8fd27c9e78cffc3d1d049'
-};
